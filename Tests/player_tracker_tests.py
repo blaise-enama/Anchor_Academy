88cfa7a0563a14b55f8bb4a4aba2ddaf808c3cd0
blaise-enama <blaise.enama@gmail.com>
2025-09-25 18:18:44 -0400

    Asserted  mysql.connector.connect was called

--- Tests/player_tracker_tests.py
+++ Tests/player_tracker_tests.py
@@ -1,6 +1,8 @@
 import unittest
+import mysql.connector
 from unittest.mock import patch, MagicMock
 from player_tracker import connect_to_database
+
 
 class TestDatabaseConncection(unittest.TestCase):
 
@@ -11,24 +13,33 @@ class TestDatabaseConncection(unittest.TestCase):
         mock_connect.return_value = mock_conn
 
         # Act
-        conn = connect_to_database('localhost','root', 'Enamfam.7', 'Anchor_Academy')
+        conn = connect_to_database('localhost','root', 'Enamfam.7', 'Anchor_Academy',driver='pymysql')
 
         # Assert
         mock_connect.assert_called_once_with(
             host="localhost",
             user="root",
-            passwd="Enamfam.7",
+            passwd="fakepassword",
             db="Anchor_Academy" # substitute with test_academy
         )
+
         self.assertEqual(conn, mock_conn)
 
 
     @patch("player_tracker.mysql.connector.connect", side_effect=Exception("Connection failed"))
-    def test_connection_failure(self, mock_connect):
+    def test_connection_failure(self,mock_connect):
+        #simulate mysql throwing an error
+        mock_connect.side_effect = mysql.connector.Error("Unable to establisn a connection")
+
         # Act
-        conn = connect_to_database("localhost", "user", "wrongpass", "test_db")
+        conn = connect_to_database(
+            "localhost", 
+            "root@localhost", 
+            "notthepassword", 
+            "test_db")
 
         # Assert
+        mock_connect.assert_called_once()
         self.assertIsNone(conn)
 
 
