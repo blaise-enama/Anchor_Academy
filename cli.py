f40dc65dee640f3a8c6902d79df33c65efd88f3f
blaise-enama <blaise.enama@gmail.com>
2026-01-16 19:06:55 -0500

    created the 'add_session' parser to the CLI

--- cli.py
+++ cli.py
@@ -10,7 +10,8 @@ def add_player(args, player_repo):
         team=args.team
     )
     
-    player_repo.add_player(player)
+    saved_player = player_repo.add_player(player)
+    print(f"Player created with ID {saved_player.player_id}")
 
 
 def display_players(player_repo):
@@ -19,7 +20,26 @@ def display_players(player_repo):
         print(p)
 
 def delete_player(args, player_repo):
-    player_repo.delete(args.player_id)
+    player_repo.delete_player(args.player_name)
+
+
+def add_session(args, session_repo):
+    session = Session(
+        player_id=args.player_id,
+        session_date=args.session_date,
+        duration_minutes=args.duration_minutes,
+        sprints=args.sprint_count,
+        distance_km=args.total_distance,
+        max_speed=args.max_speed,
+        touches_left= args.touches_left,
+        touches_right= args.touches_right
+        
+    )
+    session_repo.add(session)
+
+
+def delete_session(args, session_repo):
+    session_repo.delete_session(args.session_id)
 
 
 def main():
@@ -28,7 +48,6 @@ def main():
 
     #------- Add Player -------
     add_player_parser = subparsers.add_parser("add-player")
-    #add_player_parser.add_argument("--id", type=int, required=True)
     add_player_parser.add_argument("--name", type=str, required=True)
     add_player_parser.add_argument("--position", type=str, required=True)
     add_player_parser.add_argument("--age", type=int, required=True)
@@ -37,11 +56,25 @@ def main():
 
     # ---- Delete Player ----
     delete_player_parser = subparsers.add_parser("delete-player")
-    delete_player_parser.add_argument("--player-id", type=int, required=True)
+    delete_player_parser.add_argument("--player-name", type=str, required=True)
 
     # ---- List Players ----
     subparsers.add_parser("list-players")
     args = parser.parse_args()
+
+    # ---- Add Session ----
+    add_session_parser = subparsers.add_parser("add-session")
+    add_session_parser.add_argument("--player-id", type=int, required=True)
+    add_session_parser.add_argument("--date", required=True)
+    add_session_parser.add_argument("--duration", type=int, required=True)
+    add_session_parser.add_argument("--sprints", type=int, required = True)
+    add_session_parser.add_argument("--distance", type=float, required=True)
+    add_session_parser.add_argument("--max-speed", type=float, default=0, required=True)
+    add_session_parser.add_argument("--left-touches", type=int, default=0)
+    add_session_parser.add_argument("--right-touches", type=int, default=0)
+
+
+
 
 
     conn = connect_to_database()
@@ -53,11 +86,14 @@ def main():
 
     if args.command == "add-player":
         add_player(args, player_repo)
-        
+
     elif args.command == "list-players":
         display_players(player_repo)
     elif args.command == "delete-player":
+        #automatically deletes a player as well as their sessions via cascade
         delete_player(args, player_repo)
+    elif args.command == "add-session":
+        add_session(args, session_repo)
     else:
         parser.print_help()
 
