bedf4cc437c92f562000bd49c767425826c36cbf
blaise-enama <blaise.enama@gmail.com>
2026-02-03 14:12:24 -0500

    created tests using data from the fake repositories

--- Tests/services/test_delete_sessions.py
+++ Tests/services/test_delete_sessions.py
@@ -2,19 +2,44 @@ import pytest
 from datetime import date
 from anchor_academy.player_tracker import Session
 from anchor_academy.services.session_services import SessionService
-from tests.fakes.FakeSessionRepo import FakeSessionRepo
+from tests.fakes.session_repo import FakeSessionRepo
 
 def test_delete_session_removes_session():
-    sessions = [
-        Session(1, 1, date.today(), 60, 5000, 20, 30, 40, 60),
-        Session(2, 1, date.today(), 55, 4800, 18, 28, 50, 50),
-    ]
 
-    repo = FakeSessionRepo(sessions)
+    repo = FakeSessionRepo(
+        sessions= {
+            1: [
+                (1, 1, date.today(), 60, 20, 5000, 30, 40, 60),
+                (2, 1, date.today(), 55, 18, 4800, 28, 50, 50),
+            ]    
+        }   
+    )
     service = SessionService(repo)
-
     result = service.delete_session(1)
 
     assert result is True
-    assert len(repo.sessions) == 1
-    assert repo.sessions[0].session_id ==2
+    assert len(service.list_sessions()) == 1  
+    
+    service = SessionService(repo)
+
+
+
+def test_delete_nonexistent_session_raises_error():
+    repo = FakeSessionRepo(
+        sessions= {
+            1: [
+                (1, 1, date.today(), 60, 20, 5000, 30, 40, 60),
+                (2, 1, date.today(), 55, 18, 4800, 28, 50, 50),
+            ]    
+        }   
+    )
+    service = SessionService(repo)
+    result = service.delete_session(1)
+
+    assert result is True
+    assert len(service.list_sessions()) == 1
+
+    with pytest.raises(RuntimeError):
+        service.delete_session(999)
+    
+
---
+++ Tests/services/test_get_all_sessions.py
@@ -0,0 +1,24 @@
+import pytest
+from datetime import date
+from anchor_academy import *
+from anchor_academy.player_tracker import Session
+from anchor_academy.services.session_services import SessionService
+from tests.fakes.session_repo import FakeSessionRepo
+
+def test_get_all_sessions_returns_all_sessions():
+    #create session objects
+    sessions = [
+        Session(1, 1, date.today(), 60, 5000, 20, 30, 40, 60),
+        Session(2, 2, date.today(), 55, 4800, 18, 28, 50, 50),
+        Session(2, 1, date.today(), 70, 6200, 25, 32, 45, 55),
+    ]
+
+    repo = FakeSessionRepo(sessions)
+    service = SessionService(repo)
+
+    result = service.list_sessions()
+
+    assert len(result) == 44
+    assert result == sessions
+
+
