687047c5cc28f49b092cc9bcbd6fec213ee42367
blaise-enama <blaise.enama@gmail.com>
2026-02-18 20:25:19 -0500

    extended the fake player repository

--- fake_repos/players.py
+++ fake_repos/players.py
@@ -6,10 +6,12 @@ class FakePlayerRepo:
 
     def __init__(self):
         self.players = {
-            1: (1, "Blaise Enama", 25, "F", "Anchor Academy"),
-            2: (2, "Regis Enama", 25, "CB", "Anchor Academy"),
-            3: (3, "Joe Schmoe", 27, "CM", "BW Gottschee"),
-            4: (4, "Jane Doe",22, "CM", "Gotham FC" )
+            1: (1, "Blaise Enama", "F",25, "Anchor Academy"),
+            2: (2, "Regis Enama", "CB", 25, "Anchor Academy"),
+            3: (3, "Joe Schmoe", "CM",27, "BW Gottschee"),
+            4: (4, "Jane Doe", "CM",22, "Anchor Academy" ),
+            5: (5, "John Doe","RB", 23, "Anchor Academy"),
+            6: (6, "Nia Carter", "LW", 26, "Anchor Academy")
         }
 
     def get_by_id(self, player_id):
